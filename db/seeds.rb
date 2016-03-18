7954761aafed7339878468af45a9682b539b265c
Lionheart11 <phs.richards@hotmail.com>
2016-03-18 16:07:09 +0100

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 richard = User.create(nickname: "Richard", birthday: DateTime.new(1981,03,11), height: 180, email: "richard@wcs.fr", password: "12345678")
 melanie = User.create(nickname: "Mélanie", birthday: DateTime.new(1992,12,07), height: 180, email: "melanie@wcs.fr", password: "12345678")
 benjamin = User.create(nickname: "Benjamin", birthday: DateTime.new(1989,01,24), height: 180, email: "benjamin@wcs.fr", password: "12345678")
-valentin = User.create(nickname: "Valentin", birthday: DateTime.new(1981,03,11), height: 180, email: "valentin@wcs.fr", password: "12345678")
+valentin = User.create(nickname: "Valentin", birthday: DateTime.new(1995,03,24), height: 180, email: "valentin@wcs.fr", password: "12345678")
 suzy = User.create(nickname: "Suzy", birthday: DateTime.new(1981,03,11), height: 180, email: "suzy@wcs.fr", password: "12345678")
