562cbf5c7ed26c7074b7ad1b45b8b421a616a48b
alem972 <melanie.gourpil@gmail.com>
2016-03-21 11:37:11 +0100

    add messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316154945) do
+ActiveRecord::Schema.define(version: 20160318101320) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "author_id"
+    t.integer  "user_id"
+    t.boolean  "has_read",   default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "birthday"
-    t.string   "nickname",                            null: false
+    t.string   "nickname"
     t.integer  "height"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
