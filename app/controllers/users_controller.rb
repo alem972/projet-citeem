4515790fcacb7e4bc0deda1c4236fac0788a50cf
Benjamin <benjamin.lebas@yahoo.fr>
2016-03-23 17:46:40 +0100

    css profil edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,22 @@ class UsersController < ApplicationController
     @titre = "Édition profil"
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_param)
+      redirect_to user_path(params[:id])
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_param
+    params.require(:user).permit(:nickname, :birthday, :heigth)
+  end
+
 end
