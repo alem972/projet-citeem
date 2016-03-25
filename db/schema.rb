39863d38959488f4abe9b7bed66b139b59319cf2
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2016-03-25 13:44:40 +0100

    resolve pb css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318101320) do
+ActiveRecord::Schema.define(version: 20160323142800) do
 
   create_table "ethnic_origins", force: :cascade do |t|
     t.string   "origins"
@@ -43,12 +43,6 @@ ActiveRecord::Schema.define(version: 20160318101320) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "silhouettes", force: :cascade do |t|
-    t.string   "silhouette"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.integer  "author_id"
@@ -59,6 +53,12 @@ ActiveRecord::Schema.define(version: 20160318101320) do
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+
+  create_table "silhouettes", force: :cascade do |t|
+    t.string   "silhouette"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "birthday"
