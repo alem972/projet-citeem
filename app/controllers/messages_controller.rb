45488673e02b105fb1727815a7f2283252590674
alem972 <melanie.gourpil@gmail.com>
2016-03-25 13:28:35 +0100

    envoi des messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
 
 	def create
+		binding.pry
 		@message = Message.new(content: params[:message][:content], author_id: current_user.id, user_id: params[:message][:user_id])
 		if @message.save
-			redirect_to discution_path(@message.user.id), method: :get
+			redirect_to discution_path(params[:message][:user_id]), method: :get
 		else
 			render 'users#discution'
 		end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	@messages = Message.where(author_id: params[:id], user_id: current_user.id)
   	@messages += Message.where(author_id: current_user.id, user_id: params[:id])
-  	@messages.sort_by!{|e| e[:update_at]}
-  	# binding.pry
+  	@messages.sort_by!{|e| e[:updated_at]}
+  	
   	@message = Message.new(user_id: @user.id)
   end
 
