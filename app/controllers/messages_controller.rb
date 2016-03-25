c8a2696ce59e5b12dd1339714b9145b8db30645f
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2016-03-25 14:00:50 +0100

    add message button

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
 
 	def create
-		binding.pry
 		@message = Message.new(content: params[:message][:content], author_id: current_user.id, user_id: params[:message][:user_id])
 		if @message.save
 			redirect_to discution_path(params[:message][:user_id]), method: :get
